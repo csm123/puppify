0d8d0b4148d93fa6f026cf156cd2ff568eddc294
Corey Martin <coreym@gmail.com>
2015-02-15 19:13:07 -0500

    subscribe new users to mailchimp list

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  after_create :subscribe_user_to_mailing_list
+
+  private
+
+  def subscribe_user_to_mailing_list
+    SubscribeUserToMailingListJob.perform_later(self)
+  end
 end
