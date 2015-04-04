640c2ba812009e8987a4f47b6a5ba25ee7b596c2
Corey Martin <coreym@gmail.com>
2015-04-04 14:04:21 -0400

    notify subscribers on new video

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,8 @@
 class Video < ActiveRecord::Base
   belongs_to :category
+  after_create :notify_subscribers
+
+  def notify_subscribers
+    SendNewVideoInCategoryNotificationEmailsJob.perform_later(self)
+  end
 end
